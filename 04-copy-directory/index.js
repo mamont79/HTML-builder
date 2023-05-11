4d79ac3834768bb1f7b261368e97c1cf540429b8
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-05-11 10:57:57 +0200

    fix: task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,35 +5,35 @@ const newWay = path.join(__dirname, 'files-copy');
 
 fs.mkdir(newWay, () => { console.log('Папка успешно создана') });
 
-fs.stat(newWay, err => {
-  if(err) {
-    fs.mkdir(newWay, () => { console.log('Папка успешно создана') });
-  } 
+// fs.stat(newWay, err => {
+//   if(err) {
+//     fs.mkdir(newWay, () => { console.log('Папка успешно создана') });
+//   } 
     
-});
+// });
 
 fs.readdir(newWay, function(err, items) {
-  if (items.length > 0) {
+  if (items) {
     let delList = items;
 
     for (var i = 0; i < delList.length; i++) {
         let delFile = path.join(__dirname, 'files-copy', delList[i]);
         fs.unlink(delFile, () => { console.log('Файл успешно удалён') });
     }
-
     console.log(delList);
   }
+  fs.readdir(way, function(err, items) {
+    let copyList = items;
+    for (var i = 0; i < copyList.length; i++) {
+      let thisFile = path.join(__dirname, 'files', copyList[i]);
+      let outFile = path.join(__dirname, 'files-copy', copyList[i]);
+      fs.copyFile(thisFile, outFile, () => { console.log('Файл успешно скопирован') });
+    }
+    console.log(copyList);
+  })
 });
 
-fs.readdir(way, function(err, items) {
-  let copyList = items;
-  for (var i = 0; i < copyList.length; i++) {
-    let thisFile = path.join(__dirname, 'files', copyList[i]);
-    let outFile = path.join(__dirname, 'files-copy', copyList[i]);
-    fs.copyFile(thisFile, outFile, () => { console.log('Файл успешно скопирован') });
-  }
-  console.log(copyList);
-})
+
 
 
 
